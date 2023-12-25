6ad94966964d589d627326680f8b58efeef3e4ef
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2023-12-25 13:22:34 +0100

    feat: encode-line function (Mon, Dec 25, 2023 13:22:46 PM)

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function encodeLine(str) {
+  let arr = str.split('');
+  let arr2 = []
+  let count = 0;
+  
+  
+  
+  for(let [i, el] of arr.entries()){
+      
+       if(arr2.length === 0){
+           arr2.push(el)
+      }
+  
+      if(arr2[arr2.length-1] != el){
+  
+          if(count > 1){
+          arr2.push(count)
+          }
+          
+          arr2.push(el)
+          count = 1;
+          
+      }
+     
+      else {
+          count += 1;
+  
+      }
+      
+  }
+  
+  if(count > 1){
+          arr2.push(count)
+          }
+          
+          
+  for (let i = 0; i < arr2.length; i++) {
+  
+      if(typeof arr2[i] === 'number') {
+          arr2.splice(i-1,0,arr2[i]);
+          arr2 = arr2.slice(0,i+1).concat(arr2.slice(i+2,arr2.length))
+  
+        i++
+      }
+  }
+  
+  return arr2.join('')
+  // console.log(count)
+  }
 
 module.exports = {
   encodeLine
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+  console.log(Object.getOwnPropertyNames(date))
+
+  if (date === undefined) {
+    return 'Unable to determine the time of year!'
+  }
+
+if(Object.getOwnPropertyNames(date).length > 0 ) {
+  console.log('here')
+  return 'Invalid date!'
 }
+
+if (typeof date == 'number' || typeof date == 'string' ||  date === undefined) {
+  return 'Unable to determine the time of year!'
+}
+
+let month = date.getMonth();
+
+ if(month >= 0 && month <= 1 || month === 11) {
+  return 'winter'
+ } else if (month >= 2 && month <= 4) {
+  return 'spring'
+ } else if (month >= 5 && month <= 7) {
+  return 'summer'
+ } else if (month >= 8 && month <= 10) {
+  return 'autumn'
+ }  else {
+  return 'Invalid date!'
+ }
+}
+
+// getSeason('foo'),
+//        getSeason({ John: 'Smith' }),
+//                 getSeason(20192701),
+//                 getSeason([2019, '27', 0 + '1']),
+//               getSeason(() => new Date())
 
 module.exports = {
   getSeason
