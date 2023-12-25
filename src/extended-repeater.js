d58bc41e1c266c3d076f4baf5249ffcc8d3ea000
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2023-12-25 16:08:36 +0100

    feat:defined extended-repearter function (Mon, Dec 25, 2023 16:08:45 PM)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,62 +17,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function repeater(str, options) {
   let arr = [];
-    let arr2 = [];
+  let arr2 = [];
+
+  let defaultOptions = {repeatTimes: 0, separator: '+', addition: '', additionRepeatTimes: 0, additionSeparator: '|'}
+
+  options.repeatTimes != undefined ? defaultOptions.repeatTimes = Number(options.repeatTimes) : defaultOptions.repeatTimes = 1;
+  options.separator != undefined ? defaultOptions.separator = String(options.separator) : defaultOptions.separator = '+';
+
+  console.log(typeof String(null))
+  console.log(options.addition)
+  if (typeof options.addition != 'undefined') {
+    console.log(options.addition)
+      defaultOptions.addition = String(options.addition);
+  } else {
+    defaultOptions.addition = ''
+  }
+  // typeof options.addition != undefined ? defaultOptions.addition = 'null' : options.addition != undefined ? defaultOptions.addition = String(options.addition) : defaultOptions.addition = '';
+  // console.log(defaultOptions.addition)
+ 
+
+  options.additionRepeatTimes != undefined ? defaultOptions.additionRepeatTimes = Number(options.additionRepeatTimes) : defaultOptions.additionRepeatTimes = 1;
+  options.additionSeparator != undefined ? defaultOptions.additionSeparator = String(options.additionSeparator) : defaultOptions.additionSeparator = '|';
+
+  console.log(defaultOptions)
+
+
+
   
-    if(options.hasOwnProperty('repeatTimes') === false){
-       if(options.hasOwnProperty('addition')){
-          return str + options.addition
-           console.log(str)
-       } 
-    }
+//     if(options.hasOwnProperty('repeatTimes') === false){
+//        if(options.hasOwnProperty('addition')){
+//           return str + options.addition
+//            console.log(str)
+//        } 
+//     }
   
-    for(let i = 0; i < options.repeatTimes; i++){
+//     for(let i = 0; i < options.repeatTimes; i++){
      
-       if(options.hasOwnProperty('addition')){
-           if(options.hasOwnProperty('additionRepeatTimes')){
-                arr2 = [];
+//        if(options.hasOwnProperty('addition')){
+//            if(options.hasOwnProperty('additionRepeatTimes')){
+//                 arr2 = [];
 
-                for(let j = 0; j < options.additionRepeatTimes; j++){
-                  arr2.push(options.addition);
-                }
+//                 for(let j = 0; j < options.additionRepeatTimes; j++){
+//                   arr2.push(options.addition);
+//                 }
             
-                arr2 = str + arr2.join(options.additionSeparator);
-                arr.push(arr2);
-                console.log(arr)
-           } else {
-                arr.push(str + options.addition)
-                console.log(arr)
-           }
-       } else {
-            arr.push(str)
-            console.log(arr);
-       }
+//                 arr2 = str + arr2.join(options.additionSeparator);
+//                 arr.push(arr2);
+//                 console.log(arr)
+//            } else {
+//                 arr.push(str + options.addition)
+//                 console.log(arr)
+//            }
+//        } else {
+//             arr.push(str)
+//             console.log(arr);
+//        }
+//     }
+
+//     if(options.hasOwnProperty('separator')){
+//         arr = arr.join(options.separator);
+//     } else {
+//         arr = arr.join('+');
+//     }
+//     return arr
+// }
+  
+  for(let i = 0; i < defaultOptions.repeatTimes; i++){
+    arr2 = [];
+
+    for(let j = 0; j < defaultOptions.additionRepeatTimes; j++){
+      arr2.push(defaultOptions.addition);
     }
 
-    if(options.hasOwnProperty('separator')){
-        arr = arr.join(options.separator);
-    } else {
-        arr = arr.join('+');
-    }
-    return arr
+    arr2 = str + arr2.join(defaultOptions.additionSeparator);
+    arr.push(arr2);
+  }
+
+  arr = arr.join(defaultOptions.separator);
+
+  console.log(arr)
+
+  return arr
 }
-  // let arr = []
-  // let arr2 = []
-  // for(let i = 0; i < options.repeatTimes; i++){
-  //   arr2 = [];
 
-  //   for(let j = 0; j < options.additionRepeatTimes; j++){
-  //     arr2.push(options.addition);
-  //   }
-
-  //   arr2 = str + arr2.join(options.additionSeparator);
-  //   arr.push(arr2);
-  // }
-
-  // arr = arr.join(options.separator);
-
-
-repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' })
 
 module.exports = {
   repeater
