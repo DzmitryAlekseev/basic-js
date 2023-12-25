ce5aae2c5761a1b74da1779b58e5afc7521791c6
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2023-12-25 20:37:15 +0100

    feat: defined metrix element sum function (Mon, Dec 25, 2023 20:37:06 PM)

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,10 +16,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+let sumByRow = [];
+let temp = 0;
+
+for (let i = 0; i < matrix.length; i++) {
+  if (i === 0) {
+    sumByRow.push(matrix[i].reduce((a,b) => a + b, 0))
+  } else {
+  temp = 0;
+  for (let j = 0; j < matrix[i].length; j++){
+    if (matrix[i-1][j] != 0) {
+      temp = temp + matrix[i][j];
+    }
+  }
+  sumByRow.push(temp); }
+
 }
+
+return sumByRow.reduce((a,b) => a + b, 0);
+}
+
+matrix = [
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3]
+   ]
+
+   getMatrixElementsSum(matrix)
 
 module.exports = {
   getMatrixElementsSum
