5fd6e1c2875aec6123a6711aa11d01870d794096
DzmitryAlekseev <dmitry.alekseev.s@gmail.com>
2023-12-25 15:07:32 +0100

    feat:defined get-email.domain function (Mon, Dec 25, 2023 15:07:32 PM)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let arr = [];
+    let arr2 = [];
+  
+    if(options.hasOwnProperty('repeatTimes') === false){
+       if(options.hasOwnProperty('addition')){
+          return str + options.addition
+           console.log(str)
+       } 
+    }
+  
+    for(let i = 0; i < options.repeatTimes; i++){
+     
+       if(options.hasOwnProperty('addition')){
+           if(options.hasOwnProperty('additionRepeatTimes')){
+                arr2 = [];
+
+                for(let j = 0; j < options.additionRepeatTimes; j++){
+                  arr2.push(options.addition);
+                }
+            
+                arr2 = str + arr2.join(options.additionSeparator);
+                arr.push(arr2);
+                console.log(arr)
+           } else {
+                arr.push(str + options.addition)
+                console.log(arr)
+           }
+       } else {
+            arr.push(str)
+            console.log(arr);
+       }
+    }
+
+    if(options.hasOwnProperty('separator')){
+        arr = arr.join(options.separator);
+    } else {
+        arr = arr.join('+');
+    }
+    return arr
 }
+  // let arr = []
+  // let arr2 = []
+  // for(let i = 0; i < options.repeatTimes; i++){
+  //   arr2 = [];
+
+  //   for(let j = 0; j < options.additionRepeatTimes; j++){
+  //     arr2.push(options.addition);
+  //   }
+
+  //   arr2 = str + arr2.join(options.additionSeparator);
+  //   arr.push(arr2);
+  // }
+
+  // arr = arr.join(options.separator);
+
+
+repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
 
 module.exports = {
   repeater
--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let nStr = email.slice(email.lastIndexOf('@')+1, email.length);
+  if(nStr.startsWith('.')){
+    return nStr.slice(1, nStr.length)
+  } else {
+    return nStr;
+  }
 }
 
 module.exports = {
